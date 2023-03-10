aff95a1515fcf02944fe62f72a315790116527d8
Sofia Katzman <katzmansof@gmail.com>
2023-03-10 01:47:50 -0500

    palindrome function written for lab

--- index.js
+++ index.js
@@ -1,13 +1,28 @@
+// Write your algorithm here
 function isPalindrome(word) {
-  // Write your algorithm here
+let charArray = Array.from(word)
+let revCharArray = charArray.slice().reverse()
+if (JSON.stringify(charArray)==JSON.stringify(revCharArray)) {
+  return true;
 }
+else {
+  return false
+}
+}
+
 
 /* 
   Add your pseudocode here
-*/
+1. write function that takes a string
+2. each character in the string is pushed into an array 
+(one char per index)
+3.that array is compared to its reverse array 
+if true : it is a palindrome
+  */
 
 /*
-  Add written explanation of your solution here
+My code takes a string and converts each element into an index of an array. That array is then copied and reversed.
+Both arrays are compared using the JSON.stringify() method : if they match the function returns true, otherwise false is returned.
 */
 
 // You can run `node index.js` to view these console logs
